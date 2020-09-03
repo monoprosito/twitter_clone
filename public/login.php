b7a4d0b392e439cc07a8f2944249c2d64ec00a98
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-09-03 01:54:32 -0500

    Remove unnecessary login form inputs

--- public/login.php
+++ public/login.php
@@ -17,20 +17,12 @@
             <form action="" id="signUpForm">
                 <h1>Login on Twitter</h1>
                 <div class="row">
-                    <label for="usernameInput">Username</label>
-                    <input type="text" id="usernameInput" name="username">
-                </div>
-                <div class="row">
                     <label for="emailInput">Email</label>
                     <input type="email" id="emailInput" name="email">
                 </div>
                 <div class="row">
                     <label for="passwordInput">Password</label>
                     <input type="password" id="passwordInput" name="password">
-                </div>
-                <div class="row">
-                    <label for="phoneNumberInput">Phone</label>
-                    <input type="tel" id="phoneNumberInput" name="phoneNumber">
                 </div>
                 <span>If you don't already have an account, please <a href="<?php echo "http://localhost:8080/twitter_clone/public/signUp.php"; ?>">sign up</a>.</span>
                 <a class="primary-button">Submit</a>
