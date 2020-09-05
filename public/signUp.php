e739cd10a4821b187e42d4fa05360bcd4025bbeb
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-09-05 15:00:04 -0500

    Fixes some placeholders of the sign up form

--- public/signUp.php
+++ public/signUp.php
@@ -18,19 +18,19 @@
                 <h1>Sign up for Twitter</h1>
                 <div class="row form-group">
                     <label for="usernameInput">Username</label>
-                    <input type="text" id="usernameInput" name="username" placeholder="Enter your new username...">
+                    <input type="text" id="usernameInput" name="username" placeholder="Enter your new username, for example: johndoe01">
                 </div>
                 <div class="row form-group">
                     <label for="emailInput">Email</label>
-                    <input type="email" id="emailInput" name="email" placeholder="Enter your email...">
+                    <input type="email" id="emailInput" name="email" placeholder="Enter your email, for example: johndoe@gmail.com">
                 </div>
                 <div class="row form-group">
                     <label for="passwordInput">Password</label>
-                    <input type="password" id="passwordInput" name="password" placeholder="Enter your new password...">
+                    <input type="password" id="passwordInput" name="password" placeholder="Enter your new password, for example: A-123456">
                 </div>
                 <div class="row form-group">
                     <label for="phoneNumberInput">Phone</label>
-                    <input type="tel" id="phoneNumberInput" name="phoneNumber" placeholder="Enter your phone number...">
+                    <input type="tel" id="phoneNumberInput" name="phoneNumber" placeholder="Enter your phone number, for example: 3000000000">
                 </div>
                 <a href="<?php echo "login.php"; ?>">Already have an account?</a>
                 <a class="primary-button" id="submitForm">Submit</a>
